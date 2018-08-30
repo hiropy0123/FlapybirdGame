55d9886cbd02c8e72d1b3de0703d57473f1ab4d1
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-08-30 22:57:16 +0900

    1-6

--- FlapybirdGame/GameScene.swift
+++ FlapybirdGame/GameScene.swift
@@ -11,10 +11,23 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     // variables
+    // 画像はSKSpriteNode()で扱う
     var bird = SKSpriteNode() // player
     var gameOverImage = SKSpriteNode() // game over image
     let jumpSound = SKAction.playSoundFileNamed("sound.mp3", waitForCompletion: false) // jump sound
     let backSound = SKAction.playSoundFileNamed("backSound.mp3", waitForCompletion: false) // background sound
+    
+    var blockingObjects = SKNode() // SKNodeは画像を使わない 見えないオブジェクト
+    var score = Int(0) // スコア（タイマーの計算）
+    var scoreLabel = SKLabelNode() // スコア表示
+    var pipeTop = SKSpriteNode() // 上の障害物
+//    var pipeBottom = SKSpriteNode() // 下の障害物
+    
+    var timer: Timer = Timer() // 背景画像を動かすためのタイマー
+    var gameTimer: Timer = Timer() // ゲームの進行を管理するタイマー
+    var timeString = String() // 結果を投稿するための文字列タイム
+    
+    
     
 
     override func didMove(to view: SKView) {
