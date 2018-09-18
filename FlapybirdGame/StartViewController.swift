bd64a48eb97621ee633cdda8ea09dc1a7d0f5dba
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-09-18 23:13:41 +0900

    1-23

---
+++ FlapybirdGame/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewController.swift
+//  FlapybirdGame
+//
+//  Created by Hiroki Nakashima on 2018/09/18.
+//  Copyright © 2018年 Hiroki Nakashima. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
