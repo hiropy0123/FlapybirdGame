db3421404984816fd5f994d21c56f6edc31409f3
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-09-19 10:42:59 +0900

    1-25

--- FlapybirdGame/StartViewController.swift
+++ FlapybirdGame/StartViewController.swift
@@ -14,6 +14,7 @@ class StartViewController: UIViewController {
 
     @IBOutlet var logoImageView: UIImageView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,7 +30,47 @@ class StartViewController: UIViewController {
             self.logoImageView.frame = CGRect(x: 16, y: 143, width: 343, height: 343)
         }, completion: nil)
         
+    }
+    
+    
+    @IBAction func postLine(_ sender: Any) {
+        // LINEでシェア
+        shareLine()
         
+    }
+    
+    func shareLine() {
+        let urlscheme: String = "line://msg/text"
+        let message = timeString
+        // LINEのURLスキーム "line://msg/text/メッセージ"
+        let urlstring = urlscheme + "/" + message
+        
+        // URLエンコード
+        guard let encodedURL = urlstring.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) else {
+            return
+        }
+        
+        // URL作成
+        guard let url = URL(string: encodedURL) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(url) {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: {
+                    (succes) in
+                    // LINEアプリ表示成功
+                    
+                })
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        } else {
+            // LINEアプリがない場合
+            let alertController = UIAlertController(title: "エラーが発生しました", message: "LINEアプリがインストールさせれていません", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "OK?", style: UIAlertActionStyle.default))
+            present(alertController, animated: true, completion: nil)
+        }
     }
     
 
